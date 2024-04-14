ed21b6d07f5d534b06f6586cb09e00999abbcc5f
MadhanMetalimits <madhan@metalimits.com>
2024-04-14 22:23:33 +0530

    Implemented mail & whatsapp configuration.

--- google_sheet.js
+++ google_sheet.js
@@ -1,40 +1,20 @@
-// const scriptURL =
-//   "https://script.google.com/macros/s/AKfycbxPHifzkGuBSS2WpDEvOZwxGRP5zhVJZHok4Q_2aTPHPH4dQVLGWtfijOpPHWMbLWsy/exec";
-
-// const form = document.forms["contact-form"];
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-//     .then((response) => {
-//       console.log("****************", response);
-//       alert("Thank you! your form is submitted successfully.");
-//     })
-//     .then((response) => {
-//       //  console.log("****************", response)
-//       alert("Thank you! your form is submitted successfully.", response);
-//       //   window.location.reload();
-//     })
-//     .catch((error) => console.error("Error!", error.message));
-// });
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbw5hQZGC1XBLGrWLwEt6Qc21z9PSR0lsiGgziaOIr3kVlwhiDFXnE3OfYRuT9sHjtrg/exec";
 
 const form = document.forms["contact-form"];
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbxPHifzkGuBSS2WpDEvOZwxGRP5zhVJZHok4Q_2aTPHPH4dQVLGWtfijOpPHWMbLWsy/exec";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const formData = new FormData(form);
-
-  fetch(scriptURL, { method: "POST", body: formData })
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
-      if (response.ok) {
-        form.reset(); // Clear the form
-        form.querySelector(".sent-message").style.display = "block"; // Show the success message
-        form.querySelector(".loading").style.display = "none"; // Hide the loading message
-      } else {
-        throw new Error("Form submission failed!");
-      }
+      form.reset(); // Clear the form
+      form.querySelector(".sent-message").style.display = "block"; // Show the success message
+      form.querySelector(".loading").style.display = "none"; // Hide the loading message
+    })
+    .then((response) => {
+      //  console.log("****************", response)
+      // alert("Thank you! your form is submitted successfully.", response);
+      //   window.location.reload();
     })
     .catch((error) => {
       console.error("Error:", error);
